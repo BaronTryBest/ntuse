0862a81cb5b23fc29500f1f051267d2440705aab
PANXP <2385733147@qq.com>
2023-01-31 14:02:46 +0800

    v1.0

---
+++ ntlibrary/src/main/java/com/pxp/ntlibrary/PLibrary.java
@@ -0,0 +1,25 @@
+package com.pxp.ntlibrary;
+
+/**
+ * Created by
+ * <p>
+ * $$$$$$$\  $$\   $$\ $$$$$$$\
+ * $$  __$$\ $$ |  $$ |$$  __$$\
+ * $$ |  $$ |\$\ $$  |$$ |  $$ |
+ * $$$$$$$  | \$$$  / $$$$$$$  |
+ * $$  ____/  $$  $$<  $$  ____/
+ * $$ |      $$  /\$\ $$ |
+ * $$ |      $$ /  $$ |$$ |
+ * \__|      \__|  \__|\__|
+ * <p>
+ * time : 2023/1/30 15 44
+ */
+public class PLibrary {
+	public static String getPString(){
+		return "p-github";
+	}
+
+	public static int getPInt(){
+		return 8;
+	}
+}
